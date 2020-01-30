ee2776a67d0e4e62064721d0de9a3e27e4cd6ad6
Athanasiou <athn.nick@gmail.com>
2020-01-30 21:02:16 +0200

    commit fix

---
+++ HomeShare/Components/SearchBar.swift
@@ -0,0 +1,55 @@
+//
+//  SearchBar.swift
+//  HomeShare
+//
+//  Created by Nikolaos Athanasiou on 30/1/20.
+//  Copyright © 2020 Nikolaos Athanasiou. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @State var searchText = ""
+    @State var showCancelButton = false
+    
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+
+                TextField("search", text: $searchText, onEditingChanged: { isEditing in
+                    self.showCancelButton = true
+                }, onCommit: {
+                    print("onCommit")
+                }).foregroundColor(.primary)
+
+                Button(action: {
+                    self.searchText = ""
+                }) {
+                    Image(systemName: "xmark.circle.fill").opacity(self.searchText == "" ? 0.0 : 1.0)
+                }
+            }
+            .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+            .foregroundColor(.secondary)
+            .background(Color(.secondarySystemBackground))
+            .cornerRadius(10.0)
+
+            if showCancelButton  {
+                Button("Cancel") {
+                        UIApplication.shared.endEditing(true) // this must be placed before the other commands here
+                        self.searchText = ""
+                        self.showCancelButton = false
+                }
+                .foregroundColor(Color(.systemBlue))
+            }
+        }
+        .padding(.horizontal)
+        .navigationBarHidden(self.showCancelButton) // .animation(.default) // animation does not work properly
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar()
+    }
+}
--- HomeShare/Scenes/Search/SearchView.swift
+++ HomeShare/Scenes/Search/SearchView.swift
@@ -12,40 +12,12 @@ struct SearchView: View {
     @ObservedObject var viewModel: SearchViewModel = SearchViewModel()
     
     var body: some View {
-        VStack {
-            // Search view
-            HStack {
-                HStack {
-                    Image(systemName: "magnifyingglass")
-
-                    TextField("search", text: self.$viewModel.searchText, onEditingChanged: { isEditing in
-                        self.viewModel.showCancelButton = true
-                    }, onCommit: {
-                        print("onCommit")
-                    }).foregroundColor(.primary)
-
-                    Button(action: {
-                        self.viewModel.searchText = ""
-                    }) {
-                        Image(systemName: "xmark.circle.fill").opacity(self.viewModel.searchText == "" ? 0.0 : 1.0)
-                    }
-                }
-                .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
-                .foregroundColor(.secondary)
-                .background(Color(.secondarySystemBackground))
-                .cornerRadius(10.0)
-
-                if self.viewModel.showCancelButton  {
-                    Button("Cancel") {
-                            UIApplication.shared.endEditing(true) // this must be placed before the other commands here
-                            self.viewModel.searchText = ""
-                            self.viewModel.showCancelButton = false
-                    }
-                    .foregroundColor(Color(.systemBlue))
-                }
-            }
-            .padding(.horizontal)
-            .navigationBarHidden(self.viewModel.showCancelButton) // .animation(.default) // animation does not work properly
+        NavigationView {
+            VStack {
+                // Search view
+                SearchBar(searchText: viewModel.searchText, showCancelButton: viewModel.showCancelButton)
+                
+            }.navigationBarTitle("Search")
         }
     }
 }
