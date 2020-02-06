23a99e1714c28c9e473a4810b15f197c6fa4518a
Athanasiou <athn.nick@gmail.com>
2020-02-06 17:42:53 +0200

    fixed spacing

--- HomeShare/Components/KeyboardResponder.swift
+++ HomeShare/Components/KeyboardResponder.swift
@@ -17,26 +17,26 @@ func keyboardSensible(_ offsetValue: Binding<CGFloat>) -> some View {
       .padding(.bottom, offsetValue.wrappedValue)
       .animation(.spring())
       .onAppear {
-          NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
+            let keyWindow = UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .map({$0 as? UIWindowScene})
+                .compactMap({$0})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first
 
-              let keyWindow = UIApplication.shared.connectedScenes
-                  .filter({$0.activationState == .foregroundActive})
-                  .map({$0 as? UIWindowScene})
-                  .compactMap({$0})
-                  .first?.windows
-                  .filter({$0.isKeyWindow}).first
+            let bottom = keyWindow?.safeAreaInsets.bottom ?? 0
 
-              let bottom = keyWindow?.safeAreaInsets.bottom ?? 0
+            let value = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+            let height = value.height
 
-              let value = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
-              let height = value.height
-
-              offsetValue.wrappedValue = height - bottom
+            offsetValue.wrappedValue = height - bottom
+            
           }
 
-          NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { _ in
-              offsetValue.wrappedValue = 0
-          }
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { _ in
+            offsetValue.wrappedValue = 0
         }
+    }
     }
 }
